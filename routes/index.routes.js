26e0d60ea169e9798c3376147e17043e0e3856b8
waadshayea8@gmail.com <waadshayea8@gmail.com>
2026-09-25 15:04:23 +0300

    photographer directory

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,26 @@
 const router = require("express").Router()
+const User = require("../models/User.js")
 
 
 router.get('/',(req,res)=>{
     res.render('homepage.ejs')
 })
+
+router.get("/photographers",async(req,res)=>{
+    try{
+        const photographers = await User.find({
+            role: "photographer"
+        })
+
+        res.render("photographers/index.ejs",{
+            photographers:photographers
+        })
+
+    }catch(error){
+        console.log(error)
+        res.send("Could not load photographers.")
+    }
+})
+
+
 module.exports = router;
